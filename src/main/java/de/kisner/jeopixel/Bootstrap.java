8db3edd820f19d29822a500d9dfdff294504cd4f
Thorsten <t.kisner@web.de>
2016-08-03 20:10:36 +0200

    pom and bootstrap added

---
+++ src/main/java/de/kisner/jeopixel/Bootstrap.java
@@ -0,0 +1,49 @@
+package de.kisner.jeopixel;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.commons.configuration.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.offbytwo.jenkins.JenkinsServer;
+
+import net.sf.exlp.exception.ExlpConfigurationException;
+import net.sf.exlp.util.config.ConfigLoader;
+import net.sf.exlp.util.io.ExlpCentralConfigPointer;
+import net.sf.exlp.util.io.LoggerInit;
+
+public class Bootstrap
+{
+	final static Logger logger = LoggerFactory.getLogger(Bootstrap.class);
+
+	public final static String xmlConfig = "jeopixel/config/jbenkins.xml";
+	
+	private static Configuration config;
+	
+	public static Configuration init()
+	{
+		return init(xmlConfig);
+	}
+	
+	public static Configuration init(String configFile)
+	{
+		LoggerInit loggerInit = new LoggerInit("log4j.xml");
+		loggerInit.addAltPath("jeopixel/config");
+		loggerInit.init();
+
+		try
+		{
+			String cfn = ExlpCentralConfigPointer.getFile("jeopixel","app").getAbsolutePath();
+			ConfigLoader.add(cfn);
+			logger.info("Using additional config in: "+cfn );
+		}
+		catch (ExlpConfigurationException e) {logger.debug("No additional "+ExlpCentralConfigPointer.class.getSimpleName()+" because "+e.getMessage());}
+		ConfigLoader.add(configFile);
+		
+		config = ConfigLoader.init();
+
+		return config;
+	}
+}
